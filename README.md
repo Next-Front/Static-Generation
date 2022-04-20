e49180d900fa6cabe46f9f1b8e8a6384b4a00829
Kevin cuadros <kevindavidcuadros@gmail.com>
2022-04-20 11:34:35 -0500

    peticion para el detalle del pokemon

--- README.md
+++ README.md
@@ -6,3 +6,24 @@
 - The data comes from a headless CMS.
 - The data can be publicly cached (not user-specific).
 - The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+<hr>
+
+### getStaticPaths
+- `params` es las rutas que se aceptam
+- `fallback` en caso de que sea `false` y la pagina no existe retorna un 404, en caso de `blocking` si renderiza el componente
+```
+  export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    return {
+      paths: [
+        {
+          params: {
+            id: '1',
+          }
+        }
+      ],
+      // fallback: "blocking"
+      fallback: false
+    }
+  }
+```
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,21 +1,51 @@
 import { Layout } from "components/layouts";
-import { useRouter } from "next/router";
-
+import { GetStaticProps, NextPage, GetStaticPaths } from 'next';
+import { pokeApi } from "api";
+import { PokemonDetail } from "interface/IResponsePokeDetail";
 interface IProps {
-  pokemons: any;
+  pokemon: PokemonDetail;
 }
 
-const PokemonDetail = () => {
-
-  const { query } = useRouter();
+const PokemonDetail: NextPage<IProps> = ({ pokemon }) => {
 
   return (
     <Layout
       title=""
     >
       <h1>Pokemon Detail</h1>
+      <div>
+        <h2>{pokemon.name}</h2>
+      </div>
     </Layout>
   )
+}
+
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+
+  const pokemons = [...Array(151)].map( (_, i) => `${i + 1}` );
+
+  return {
+    paths: pokemons.map( (id) => ({
+      params: {
+        id
+      }
+    })),
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  
+  const { id } = params as { id: string };
+
+  const { data } = await pokeApi.get<PokemonDetail>(`/pokemon/${id}`);
+
+  return {
+    props: {
+      pokemon: data
+    }
+  }
 }
 
 export default PokemonDetail
